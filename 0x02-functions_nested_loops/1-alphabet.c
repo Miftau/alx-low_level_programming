74163a928c38de2308e6df7371f9bf2b4616173c
Yttrium <77174957+Miftau@users.noreply.github.com>
2022-09-13 13:19:28 +0100

    Initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet - prints lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
