1790f4bd914e98d74f2e6fed2bc62c39b992190f
Yttrium <77174957+Miftau@users.noreply.github.com>
2022-09-23 00:06:17 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,45 +1,23 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: string to be appended to
- * @src: string to append
- * Return: concatenated string
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ *
+ * Return: address of dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (dest[i] != '\0')
+	i = j = 0;
+	while (*(dest + i))
 		i++;
-
-	while (src[j] != '\0')
+	while ((*(dest + i) = *(src + j)))
 	{
-		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
-
 	return (dest);
 }
-
-/* another method using pointer notation
-char *_strcat(char *dest, char *src)
-{
-        int i = 0;
-	int j = 0;
-	while (*(dest + i))
-	   i++;
-	while (*(src + j))
-	{
-	   *(dest+i) = *(src+j);
-	   i++;
-	   j++;
-	}
-	*(dest+i) = '\0';
-	return (dest);
-}
-*/
