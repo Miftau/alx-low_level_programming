7abab1207f8639fcf53457c449ef38bb5bbb612f
Yttrium <77174957+Miftau@users.noreply.github.com>
2022-09-22 23:52:05 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,38 +1,17 @@
 #include "main.h"
 
 /**
- * _strcmp - compare two strings
- * @s1: one string
- * @s2: one string
- * Return: int that tells num spaces in between
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: outputs the difference of s1 and s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 		i++;
-
-	if (*(s2 + i))
-		return (*(s1 + i) - *(s2 + i));
-	else
-		return (0);
+	return (*(s1 + i) - *(s2 + i));
 }
-
-/* another method
-int _strcmp(char *s1, char *s2)
-{
-        // while neither strings have ended
-        while (*s1 != '\0' || *s2 != '\0')
-        {
-		// if element in arr1 in arr2 are diff, return difference
-                if (*s1 != *s2)
-                        return (*s1 - *s2);
-		// iterate index
-                s1++;
-                s2++;
-        }
-        return (0);
-}
-*/
