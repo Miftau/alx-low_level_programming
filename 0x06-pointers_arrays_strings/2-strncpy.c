d65f98c1bd76d7d5fcaf969160240aaf386c656b
Yttrium <77174957+Miftau@users.noreply.github.com>
2022-09-23 00:01:29 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,39 +1,27 @@
 #include "main.h"
 
 /**
- * _strncpy - copies n bytes of a source into buffer of a destination string,
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * @n: n bytes to be copied
- * Return: copied string
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: string to copy to
+ * @src: string being copied
+ * @n: largest number of bytes to copy
+ *
+ * Return: address of dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	while (i < n)
+	i = 0;
+	while (i < n && *(src + i))
 	{
-		dest[i] = '\0';
+		*(dest + i) = *(src + i);
 		i++;
 	}
-
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
-
-/* this method in pointer arithmetic
-char *_strncpy(char *dest, char *src, int n)
-{
-  int i;
-  for (i = 0; i < n && *(src + i); i++)
-      *(dest + i) = *(src + i);
-  while (i < n)
-  {
-      *(dest + i) = '\0';
-      i++;
-  }
-  return (dest);
-}
-*/
